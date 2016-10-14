3692523fe129cb1d790b55f9f124bda80f06a9f0
Gystark <gystark@gmail.com>
2016-10-14 22:35:15 +0100

    Created models.

--- DoYouHaveTheGuts/DoYouHaveTheGuts/settings.py
+++ DoYouHaveTheGuts/DoYouHaveTheGuts/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'o&5_pncyvpeo3&@^mg78jgra7y$$i2bcy@cj@^$a__$-p(#^b('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
