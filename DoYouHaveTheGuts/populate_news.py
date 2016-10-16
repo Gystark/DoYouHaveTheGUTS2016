386e0a201cbf212de2b952021cf597a59f884630
georgikoyrushki95 <georgi.koyrushki@gmail.com>
2016-10-16 01:16:44 +0100

    boolean added to save in news

--- DoYouHaveTheGuts/populate_news.py
+++ DoYouHaveTheGuts/populate_news.py
@@ -5,6 +5,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DoYouHaveTheGuts.settings')
 django.setup()
 from pss.models import News
 
+
 def populate():
     news_file = open('news.txt', 'r')
 
