57913bf9017f80f4ad2820e1622672caecc0c101
georgikoyrushki95 <georgi.koyrushki@gmail.com>
2016-10-16 01:57:48 +0100

    before jumping to gabor's computer

--- DoYouHaveTheGuts/templates/notifications/includes/delete_success.js
+++ DoYouHaveTheGuts/templates/notifications/includes/delete_success.js
@@ -3,8 +3,8 @@ var deleteSuccess = function (response, notification) {
     var $selected_notification = notification.closest(nfClassSelector);
     $selected_notification.fadeOut(300, function () {
         $(this).remove();
-        // //update the number of notifications
-        // var dom_not_number = $('#notifications-number');
-        // dom_not_number.html(dom_not_number.html() - 1);
+        //update the number of notifications
+        var dom_not_number = $('#notifications-number');
+        dom_not_number.html(dom_not_number.html() - 1);
     });
 };
